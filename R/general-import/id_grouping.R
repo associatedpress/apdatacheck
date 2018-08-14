ac9e712aa0264184d56e17876da40d9a2f7be94e
Michelle Minkoff <mminkoff@ap.org>
2018-08-14 14:55:09 -0400

    Adds basic data checks and code to handle checking for duplicate ids

---
+++ R/general-import/id_grouping.R
@@ -0,0 +1,34 @@
+library(dplyr)
+library(readr)
+
+id_grouping <- function(df, filepath, id_column, verbose=FALSE) {
+  grouped_ids <- df %>% group_by(NAT_ID) %>% summarize(count = n())
+  number_of_ids <- nrow(grouped_ids)
+  duped_ids <- grouped_ids %>% filter(count != 1)
+  if (nrow(duped_ids) != 0) {
+  warning(paste0(
+    "id_grouping: ", filepath, ":\n",
+    "    You have ", nrow(duped_ids), " duplicated ids which are ", duped_ids %>% select_(id_column))
+  )
+  } else {
+    message(paste0(
+      "id_grouping: ", filepath, ":\n",
+      "    All ", number_of_ids, " ids are unique!")
+    )
+  }
+}
+
+test_id_grouping <- function() {
+  # Individual tests here
+  filepath <- "testdata/id_grouping/mi_dams_data.csv"
+  df <- read_csv(filepath)
+  id_grouping(df, filepath, "NAT_ID", verbose=TRUE)
+
+  filepath <- "testdata/id_grouping/mi_dams_data_dupes.csv"
+  df <- read_csv(filepath)
+  id_grouping(df, filepath, "NAT_ID", verbose=TRUE)
+
+  x <- ""
+}
+
+test_id_grouping()
