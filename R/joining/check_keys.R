0be2d938341fb09755478b01d89a0aaf8c3644e5
lfenn <lfenn@ap.org>
2019-04-11 12:43:36 -0400

    Implementation of the basic 'join' checks

---
+++ R/joining/check_keys.R
@@ -0,0 +1,51 @@
+library(tidyverse)
+
+# Like other join checks, we don't return any type here, just messages
+check_keys <- function(left, right, by = by, verbose = F, join_type = "left") {
+  left_nondistinct <- left %>% count_(by) %>% filter(n > 1)
+  right_nondistinct <- right %>% count_(by) %>% filter(n > 1)
+  left_primary_key_valid <- nrow(left_nondistinct) == 0
+  right_primary_key_valid <- nrow(right_nondistinct) == 0
+
+  warning_string <-
+    paste0(
+      "\ncheck_keys: ", by, ":\n",
+      "    Key given is not a primary key")
+  if(join_type == "left") {
+    warning_string_verbose <-
+      paste0(
+        warning_string,
+        "\n    Right: ", nrow(right_nondistinct), " nondistinct row(s)"
+      )
+  }
+  if(join_type == "right") {
+    warning_string_verbose <-
+      paste0(
+        warning_string,
+        "\n    Left: ", nrow(left_nondistinct), " nondistinct row(s)"
+      )
+  }
+  if(join_type == "full") {
+    warning_string_verbose <-
+      paste0(
+        warning_string,
+        "\n    Left: ", nrow(left_nondistinct), " nondistinct row(s)\n",
+        "    Right: ", nrow(right_nondistinct), " nondistinct row(s)"
+      )
+  }
+  if(left_primary_key_valid & join_type == "right" |
+     right_primary_key_valid & join_type == "left") {
+    message("check_keys: pass")
+  } else {
+    if(verbose) {
+      warning(warning_string_verbose)
+    } else {
+      warning(warning_string)
+    }
+  }
+}
+
+# Tests:
+# Check the keys are actually in the dang df
+# Check the dfs aren't null
+# check_keys(mtcars, mtcars, c("mpg", "hp"))
---
+++ R/joining/count_rows.R
@@ -0,0 +1,53 @@
+
+# This function should have no return; it will be called entirely for its
+# warnings
+# Is this redundant with check_keys?
+count_rows <- function(left, right, by = NULL, join_type) {
+  left_rows <- left %>% nrow()
+  right_rows <- right %>% nrow()
+  if(join_type == "left") {
+    # The danger in the left join is that the right-hand frame has duplicate rows
+    # the check for this is to make sure the resulting dataframe has the same
+    # number of rows as the left input dataframe
+    row_count_change <- left %>% left_join(right, by) %>% nrow() - left_rows
+  }
+  if(join_type == "right") {
+    row_count_change <- left %>% right_join(right, by) %>% nrow() - right_rows
+  }
+  # The filtering joins are both about dropping rows selectively. We will try
+  # to report and warn about cases where nothing changes.
+  if(join_type == "semi") {
+    row_count_change <- left_rows - left %>% semi_join(right, by) %>% nrow()
+  }
+  if(join_type == "anti") {
+    row_count_change <- left_rows - left %>% anti_join(right, by) %>% nrow()
+  }
+
+  # Types of returns
+  # For left, right joins, strong expectation that nrow is the same
+  # For semi, anti joins, mild expectation that nrow should change
+  # For full, inner joins, just report change
+  if(join_type %in% c("left", "right")) {
+    if(row_count_change == 0) {
+      message("count_rows: pass")
+    } else {
+      warning_string <-
+        paste0(
+          "\ncount_rows: ", paste(by, collapse = ', '), ":\n",
+          "    ", row_count_change, " row(s) added after join")
+      warning(warning_string)
+    }
+  }
+  if(join_type %in% c("semi", "anti")) {
+    if(row_count_change != 0) {
+      message("count_rows: pass")
+    } else {
+      warning_string <-
+        paste0(
+          "\ncount_rows: ", paste(by, collapse = ', '), ":\n",
+          "    No rows were dropped")
+      warning(warning_string)
+    }
+  }
+}
+# count_rows(mtcars, mtcars, c('mpg', 'hp', 'disp'), 'left')
