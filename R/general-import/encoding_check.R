e7fdbbbbb29cd574b66bf7222b5f6929da24aded
lfenn <lfenn@ap.org>
2019-05-30 13:51:41 -0400

    Encoding check start of file

---
+++ R/general-import/encoding_check.R
@@ -0,0 +1,11 @@
+# Look for specific strings that really don't exist in the data sets we use
+# - make this an argument?
+#
+# Once we find a suspicious string...
+
+# Return data frame where the rows are all of the suspicious rows? (w/ row index)
+# If this data frame is empty pass
+
+encoding_check <- function(df, bad_seqs = c("Ã©", "Ã±")) {
+
+}
