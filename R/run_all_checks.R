35599a61d6488ae048f38a22d5a0ebbbbf19da13
lfenn <lfenn@ap.org>
2019-06-06 13:21:58 -0400

    Correctly export/import functions

--- R/run_all_checks.R
+++ R/run_all_checks.R
@@ -1,10 +1,6 @@
 #' Load the data checking functions as well as a high level
 #' "run all checks" function.
-#' @param dataframe The data frame to run checks against
-#' @export
-#' @examples
-#' run_all_checks(mtcars)
-
+#' @import tidyverse
 library(tidyverse)
 source('R/general-import/check_nas.R')
 source('R/general-import/content_spot_check.R')
@@ -20,6 +16,7 @@ source('R/joining/count_rows.R')
 # test_multifile_load()
 # test_length_match()
 
+#' @export
 import_checks <- function(dataframe, filename) {
   dataframe %>%
     length_match(filename)
@@ -27,36 +24,42 @@ import_checks <- function(dataframe, filename) {
 
 # Join functions -----
 # define our new join functions
+#' @export
 left_join_dc <- function(left, right, by = NULL, verbose = F) {
   check_keys(left, right, by, verbose)
   count_rows(left, right, by, 'left')
   left_join(left, right, by)
 }
 
+#' @export
 right_join_dc <- function(left, right, by = NULL, verbose = F) {
   check_keys(left, right, by, verbose)
   count_rows(left, right, by, 'right')
   right_join(left, right, by)
 }
 
+#' @export
 inner_join_dc <- function(left, right, by = NULL, verbose = F) {
   check_keys(left, right, by, verbose)
   count_rows(left, right, by, 'inner')
   inner_join(left, right, by)
 }
 
+#' @export
 full_join_dc <- function(left, right, by = NULL, verbose = F) {
   check_keys(left, right, by, verbose)
   count_rows(left, right, by, 'full')
   full_join(left, right, by)
 }
 
+#' @export
 semi_join_dc <- function(left, right, by = NULL, verbose = F) {
   check_keys(left, right, by, verbose)
   count_rows(left, right, by, 'semi')
   semi_join(left, right, by)
 }
 
+#' @export
 anti_join_dc <- function(left, right, by = NULL, verbose = F) {
   check_keys(left, right, by, verbose)
   count_rows(left, right, by, 'anti')
@@ -64,6 +67,7 @@ anti_join_dc <- function(left, right, by = NULL, verbose = F) {
 }
 
 # Run all checks -----
+#' @export
 run_all_checks <- function(dataframe) {
   dataframe %>%
     run_import_checks() %>%
