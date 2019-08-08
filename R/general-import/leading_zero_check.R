d2b4f67947de1e8dbda7ebba99aad5c527f6c245
lfenn <lfenn@ap.org>
2019-08-08 13:34:23 -0400

    Put in Qs from the checklist sheet

--- R/general-import/leading_zero_check.R
+++ R/general-import/leading_zero_check.R
@@ -1,3 +1,6 @@
+# Are there any leading zeros being dropped off data (ZIP codes!)?
+# Is such data being interpreted as strings (ZIP codes should not be numbers)?
+
 leading_zero_check <- function(...) {
 
 }
